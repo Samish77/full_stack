b684c94d74a746100ff342ba29f3eb51ab1a047a
sam <samish707@gmail.com>
2024-02-21 22:53:09 +0530

    fiz bizz

--- java_script/javascript.js
+++ java_script/javascript.js
@@ -1,7 +1,20 @@
 // use of Ananomus function and Arrow function
-const textBox=document.querySelector("#textBox");
-const output=document.querySelector("#output");
+// const textBox=document.querySelector("#textBox");
+// const output=document.querySelector("#output");
 
-textBox.addEventListener("keydown",(Event)=>{
-    output.textContent=`You pressed "${Event.key}".`;
-});
+// textBox.addEventListener("keydown",(Event)=>{
+    // output.textContent=`You pressed "${Event.key}".`;
+// });
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
