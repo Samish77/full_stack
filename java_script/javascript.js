6c35b28429b39b4376735129fa00f269dea85471
sam <samish707@gmail.com>
2024-04-20 15:43:54 +0530

    javascript

--- java_script/javascript.js
+++ java_script/javascript.js
@@ -6,15 +6,32 @@
     // output.textContent=`You pressed "${Event.key}".`;
 // });
 
-let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-for (let i = 1; i <= answer; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-  }
+//let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+//for (let i = 1; i <= answer; i++) {
+//    if (i % 3 === 0 && i % 5 === 0) {
+//      console.log("FizzBuzz");
+//    } else if (i % 3 === 0) {
+//      console.log("Fizz");
+//    } else if (i % 5 === 0) {
+//      console.log("Buzz");
+//    } else {
+//      console.log(i);
+//    }
+//  }
+fullname="Tony Stark";
+age=24;
+price=99.99;
+radius=14;
+a=null;
+y=undefined;
+isFollow=true;
+console.log(isFollow)
+console.log(price)
+console.log(radius)
+
+const student{
+age: 20,
+name: "Samish",
+
+
+}
